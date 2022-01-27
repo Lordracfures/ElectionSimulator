2bcaf243d60ae3ff442dad979066a3423c0c89c9
Lordracfures <rauckfurer@gmail.com>
2022-01-27 12:35:53 +0100

    restos

--- src/main/java/com/example/mvc/controlador/Votante.java
+++ src/main/java/com/example/mvc/controlador/Votante.java
@@ -1,23 +1,30 @@
 package com.example.mvc.controlador;
 
-import com.example.mvc.modelo.Comunidad;
-
 public class Votante extends Thread{
 
 
-    private Comunidad comunidad;
 
 
-    public Votante(Comunidad comunidad) {
-        this.comunidad = comunidad;
 
 
-    }
+
+
+
+
 
     @Override
     public void run() {
 
+
+
+
+
+
+
+
     }
+
+
 
 
 
